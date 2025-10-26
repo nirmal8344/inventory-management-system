d59b651424593b6be303c5024ccc78f22dcd971e
Nirmal <kumarrnk712@gmail.com>
2025-10-26 12:32:09 +0530

    fix(frontend): configure tailwind content path correctly

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // INTHA 'content' SECTION DHAN ROMBA MUKKIYAM
+  // Namma code ellam "src" folder kulla iruku nu inga theliva solrom
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
